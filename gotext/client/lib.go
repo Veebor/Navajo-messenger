e205c2d1f8942f40c228b98df6f94326786bf3ac
CFV <vescovocarlofederico@gmail.com>
2019-07-21 23:53:29 +0200

    New client to TEST

---
+++ gotext/client/lib.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
+type ChatRequest struct {
+	Action   string
+	Username string
+	Message  string
+}
+
+var saddr *net.UDPAddr
+
+/*
+func hashSHA256(s string) string {
+	hash := sha256.Sum256([]byte(s))
+	return string(hash[:])
+}
+*/
+
+func startServerConn(serverAddress string, localport string) *net.UDPConn {
+	saddr, _ = net.ResolveUDPAddr("udp4", serverAddress)
+	// Prepare for local listening.
+	addr, err := net.ResolveUDPAddr("udp4", localport)
+	if err != nil {
+		log.Print("Resolve local address failed.")
+		log.Fatal(err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Print("Listen UDP failed.")
+		log.Fatal(err)
+	}
+	return conn
+}
+
+func registerPeer(conn *net.UDPConn) {
+	buf := make([]byte, 2048)
+	initChatRequest := ChatRequest{
+		"New",
+		username,
+		"",
+	}
+	jsonRequest, err := json.Marshal(initChatRequest)
+	if err != nil {
+		log.Print("Marshal Register information failed.")
+		log.Fatal(err)
+	}
+	_, err = conn.WriteToUDP(jsonRequest, saddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("Waiting for server response...")
+	_, _, err = conn.ReadFromUDP(buf)
+	if err != nil {
+		log.Print("Register to server failed.")
+		log.Fatal(err)
+	}
+}
+
+func getPeerAddr(conn *net.UDPConn, peer string) *net.UDPAddr {
+	buf := make([]byte, 2048)
+	connectChatRequest := ChatRequest{
+		"Get",
+		username,
+		peer,
+	}
+	jsonRequest, err := json.Marshal(connectChatRequest)
+	if err != nil {
+		log.Print("Marshal connection information failed.")
+		log.Fatal(err)
+	}
+	var serverResponse ChatRequest
+	for i := 0; i < 3; i++ {
+		_, err = conn.WriteToUDP(jsonRequest, saddr)
+		if err != nil {
+			log.Println("WriteToServer Error", err)
+		}
+		n, _, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Print("Get peer address from server failed.")
+			log.Fatal(err)
+		}
+		err = json.Unmarshal(buf[:n], &serverResponse)
+		if err != nil {
+			log.Print("Unmarshal server response failed.")
+			log.Fatal(err)
+		}
+		if strings.Contains(serverResponse.Message, "IDINUSE") {
+			fmt.Println("Peer ID already in use")
+			os.Exit(0)
+		} else if serverResponse.Message != "" {
+			break
+		}
+		time.Sleep(10 * time.Second)
+	}
+	if serverResponse.Message == "" {
+		log.Fatal("Cannot get peer's address")
+	}
+	log.Print("Peer address: ", serverResponse.Message)
+	peerAddr, err := net.ResolveUDPAddr("udp4", serverResponse.Message)
+	if err != nil {
+		log.Print("Resolve peer address failed.")
+		log.Fatal(err)
+	}
+	return peerAddr
+}
+
+func SetupCloseHandler(conn *net.UDPConn) {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\r- Ctrl+C pressed in Terminal")
+		cleanQuit(conn)
+	}()
+}
+
+func cleanQuit(conn *net.UDPConn) {
+	fmt.Print("Quitting...")
+	messageRequest := ChatRequest{
+		"RM",
+		username,
+		"",
+	}
+	jsonRequest, err := json.Marshal(messageRequest)
+	if err != nil {
+		log.Print("Error: ", err)
+	}
+	_, _ = conn.WriteToUDP(jsonRequest, saddr)
+	os.Exit(0)
+}
+
+func write(conn *net.UDPConn, message string, peerAddr *net.UDPAddr) {
+		if strings.Contains(message, "_exit0") {
+			send(conn, "Chat", "_quitting...0", peerAddr)
+			send(conn, "RM", "", saddr)
+			os.Exit(0)
+		} else {
+			send(conn, "Chat", message, peerAddr)
+		}
+}
+
+func send(conn *net.UDPConn, action string, message string, peerAddr *net.UDPAddr) {
+	messageRequest := ChatRequest{
+		action,
+		username,
+		message,
+	}
+	jsonRequest, err := json.Marshal(messageRequest)
+	if err != nil {
+		log.Print("Error: ", err)
+	}
+	_, err = conn.WriteToUDP(jsonRequest, peerAddr)
+	if err != nil {
+		log.Print("Error: ", err)
+	}
+}
+
+func listen(conn *net.UDPConn) (string, string) {
+	buf := make([]byte, 4096)
+	n, _, err := conn.ReadFromUDP(buf)
+	if err != nil {
+		log.Print(err)
+	}
+	// log.Print("Message from ", addr.IP)
+	var message ChatRequest
+	err = json.Unmarshal(buf[:n], &message)
+	if err != nil {
+		log.Print(err)
+	}
+	return message.Username, message.Message
+}
--- gotext/client/main.go
+++ gotext/client/main.go
@@ -1,207 +1,50 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	"time"
 )
 
-type ChatRequest struct {
-	Action   string
-	Username string
-	Message  string
-}
-
 var username string
-var saddr *net.UDPAddr
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if len(os.Args) < 5 {
 		log.Fatal("Usage: ", os.Args[0], " port serverAddr username peername")
 	}
-	port := fmt.Sprintf(":%s", os.Args[1])
+	portLoc := fmt.Sprintf(":%s", os.Args[1])
 	serverAddr := os.Args[2]
 	username = os.Args[3]
 	peer := os.Args[4]
-	buf := make([]byte, 2048)
 
-	// Prepare to register user to server.
-	saddr, _ = net.ResolveUDPAddr("udp4", serverAddr)
+	conn := startServerConn(serverAddr, portLoc)
 
-	// Prepare for local listening.
-	addr, err := net.ResolveUDPAddr("udp4", port)
-	if err != nil {
-		log.Print("Resolve local address failed.")
-		log.Fatal(err)
-	}
-	conn, err := net.ListenUDP("udp", addr)
-	if err != nil {
-		log.Print("Listen UDP failed.")
-		log.Fatal(err)
-	}
+	registerPeer(conn)
 
 	SetupCloseHandler(conn)
 
-	// Send registration information to server.
-	initChatRequest := ChatRequest{
-		"New",
-		username,
-		"",
-	}
-	jsonRequest, err := json.Marshal(initChatRequest)
-	if err != nil {
-		log.Print("Marshal Register information failed.")
-		log.Fatal(err)
-	}
-	_, err = conn.WriteToUDP(jsonRequest, saddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Print("Waiting for server response...")
-	_, _, err = conn.ReadFromUDP(buf)
-	if err != nil {
-		log.Print("Register to server failed.")
-		log.Fatal(err)
-	}
-
-	// Send connect request to server
-	connectChatRequest := ChatRequest{
-		"Get",
-		username,
-		peer,
-	}
-	jsonRequest, err = json.Marshal(connectChatRequest)
-	if err != nil {
-		log.Print("Marshal connection information failed.")
-		log.Fatal(err)
-	}
-
-	var serverResponse ChatRequest
-	for i := 0; i < 3; i++ {
-		conn.WriteToUDP(jsonRequest, saddr)
-		n, _, err := conn.ReadFromUDP(buf)
-		if err != nil {
-			log.Print("Get peer address from server failed.")
-			log.Fatal(err)
-		}
-		err = json.Unmarshal(buf[:n], &serverResponse)
-		if err != nil {
-			log.Print("Unmarshal server response failed.")
-			log.Fatal(err)
-		}
-		if strings.Contains(serverResponse.Message, "IDINUSE") {
-			fmt.Println("Peer ID already in use")
-			os.Exit(0)
-		} else if serverResponse.Message != "" {
-			break
-		}
-		time.Sleep(10 * time.Second)
-	}
-	if serverResponse.Message == "" {
-		log.Fatal("Cannot get peer's address")
-	}
-	log.Print("Peer address: ", serverResponse.Message)
-	peerAddr, err := net.ResolveUDPAddr("udp4", serverResponse.Message)
-	if err != nil {
-		log.Print("Resolve peer address failed.")
-		log.Fatal(err)
-	}
+	peerAddress := getPeerAddr(conn, peer)
 
 	// Start chatting.
-	write(conn, peerAddr)
-	go listen(conn)
-
-}
-
-func SetupCloseHandler(conn *net.UDPConn) {
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	fmt.Println("Connected to", peer)
 	go func() {
-		<-c
-		fmt.Println("\r- Ctrl+C pressed in Terminal")
-		messageRequest := ChatRequest{
-			"RM",
-			username,
-			"",
+		for {
+			message := make([]byte, 4096)
+			fmt.Print("> ")
+			fmt.Scanln(&message)
+			write(conn, string(message), peerAddress)
 		}
-		jsonRequest, err := json.Marshal(messageRequest)
-		if err != nil {
-			log.Print("Error: ", err)
-		}
-		_, _ = conn.WriteToUDP(jsonRequest, saddr)
-		os.Exit(0)
 	}()
-}
-
-func write(conn *net.UDPConn, peerAddr *net.UDPAddr) {
-	for {
-		fmt.Print("Input message: ")
-		message := make([]byte, 4096)
-		fmt.Scanln(&message)
-		if strings.Contains(string(message), "_exit0") {
-			sendChat(conn, "quitting...", peerAddr)
-			messageRequest := ChatRequest{
-				"RM",
-				username,
-				"",
+	go func() {
+		for {
+			remUser, remMessage := listen(conn)
+			fmt.Println(remUser, "said: ", remMessage)
+			if remMessage == "_quitting...0" {
+				cleanQuit(conn)
 			}
-			jsonRequest, err := json.Marshal(messageRequest)
-			if err != nil {
-				log.Print("Error: ", err)
-			}
-			_, _ = conn.WriteToUDP(jsonRequest, saddr)
-			os.Exit(0)
+			fmt.Print("> ")
 		}
-		sendChat(conn, string(message), peerAddr)
-	}
-}
-
-func sendChat(conn *net.UDPConn, message string, peerAddr *net.UDPAddr) {
-	messageRequest := ChatRequest{
-		"Chat",
-		username,
-		message,
-	}
-	jsonRequest, err := json.Marshal(messageRequest)
-	if err != nil {
-		log.Print("Error: ", err)
-	}
-	_, _ = conn.WriteToUDP(jsonRequest, peerAddr)
-}
-
-func listen(conn *net.UDPConn) {
-	for {
-		buf := make([]byte, 4096)
-		_, _, err := conn.ReadFromUDP(buf)
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		// log.Print("Message from ", addr.IP)
-		var message ChatRequest
-		err = json.Unmarshal(buf, &message)
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-		if strings.Contains(message.Message, "quitting...") {
-			fmt.Println("Exit command received")
-			fmt.Println("Do you want to quit? (Y/N)")
-			var confirmExit string
-			_, _ = fmt.Scanln(&confirmExit)
-			if confirmExit == "Y" {
-				os.Exit(0)
-			} else {
-				continue
-			}
-		}
-		fmt.Println(message.Username, ":", message.Message)
-	}
+	}()
+	select {}
 }
